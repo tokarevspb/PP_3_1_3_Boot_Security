98bbcf5d9cd5f89ff4bbf68046ae2fc731742cbd
Сергей Андреевич Токарев <tokarev@niuitmo.ru>
2022-12-25 15:52:22 +0400

    fixed full imports

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
@@ -3,7 +3,7 @@ package ru.kata.spring.boot_security.demo.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
 import ru.kata.spring.boot_security.demo.services.UserService;
 
 import java.security.Principal;
